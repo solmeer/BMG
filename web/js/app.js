50c80425940824b6f76b6942494de00d842f4ccf
Eughene Rymar <eughene87@ukr.net>
2017-03-22 23:55:14 +0200

    some edits in app.js

--- web/js/app.js
+++ web/js/app.js
@@ -1,10 +1,52 @@
-var app = angular.module("app",[]);
+var app = angular.module("frontendApp",['ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+
+var origin = document.location.origin;
+var folder = document.location.pathname.split('/')[1];
+
+var path = origin + "/" + folder + "views/";
+
+app.
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+            templateUrl: path + 'index.html',
+            controller: 'CarouselDemoCtrl'
+        }
+    ).when('/article',{
+            templateUrl: path + 'about.html',
+            controller: 'articleController'
+        }
+    ).otherwise(
+        {
+            redirectTo: '/'
+        }
+    );
+}]);
 
 app.controller("getNews", function ($scope, $http) {
-    $http.get("http://bmgt.herokuapp.com/api/articles/")
+    $http.get("/api/articles/")
         .then(function(response){
             $scope.newsArray = response.data;
 
 
         });
+});
+
+app.controller('CarouselDemoCtrl', function ($scope) {
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var currIndex = 0;
+
+    $scope.addSlide = function() {
+        slides.push({
+            image: 'img/header' + i + '.jpg',
+            id: currIndex++
+        });
+    };
+
+    for (var i = 1; i < 4; i++) {
+        $scope.addSlide();
+    }
+
 });
