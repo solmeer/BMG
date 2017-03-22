cbb75e99fb390536fa367c43989d96d78e8a91db
Eughene Rymar <eughene87@ukr.net>
2017-03-22 22:43:37 +0200

    create folders for assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,33 @@ var gulp         = require('gulp'),
     del          = require('del');
 
 gulp.task('js', function() {
-    return gulp.src([
-        'bower_components/jquery/dist/jquery.js',
-        'bower_components/bootstrap/dist/js/bootstrap.js'
-    ])
+    return gulp.src(['ui/js/*.js'])
         .pipe(gulp.dest('web/js'));
 });
 
 gulp.task('css', function() {
-    return gulp.src([
-        'bower_components/bootstrap/dist/css/*',
-        'web-src/base-template.css'
-    ])
+    return gulp.src(['ui/css/*.css'])
         .pipe(gulp.dest('web/css/'));
 });
 
 gulp.task('fonts', function () {
-    return gulp.src(['bower_components/bootstrap/fonts/*'])
+    return gulp.src(['ui/css/fonts/*.ttf'])
         .pipe(gulp.dest('web/fonts/'))
 });
 
+gulp.task('img', function () {
+    return gulp.src(['ui/img/*'])
+        .pipe(gulp.dest('web/img/'))
+});
+
 gulp.task('clean', function () {
-    del(['js', 'fonts', 'css']);
+    del(['js', 'fonts', 'css', 'img']);
 });
 
 gulp.task('default', function () {
     var tasks = [
         'clean',
+        'img',
         'fonts',
         'css',
         'js'
