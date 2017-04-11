e2771528f2f477e75a3030c775b7015a34f2cf20
Eughene Rymar <eughene87@ukr.net>
2017-04-12 01:20:06 +0300

    try to remove hashbang in angular url

--- web/js/app.js
+++ web/js/app.js
@@ -7,7 +7,7 @@ var path = origin + "/" + folder + "views/";
 
 app.config(['$routeProvider','$locationProvider', function ($routeProvider,$locationProvider) {
 
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true).hashPrefix('!');
 
     $routeProvider.when('/', {
             templateUrl: path + 'index.html',
