e247cd776672dca8af5454b71000b7d4eeae92ed
Eughene Rymar <eughene87@ukr.net>
2017-04-12 00:11:17 +0300

    edit heroku_parameters.php

--- app/config/heroku_parameters.php
+++ app/config/heroku_parameters.php
@@ -16,3 +16,4 @@
     $container->setParameter('aws_region', getenv('AWS_REGION'));
     $container->setParameter('aws_key', getenv('AWS_ACCESS_KEY_ID'));
     $container->setParameter('aws_secret_key', getenv('AWS_SECRET_ACCESS_KEY'));
+    $container->setParameter('aws_bucket', getenv('AWS_BUCKET'));
